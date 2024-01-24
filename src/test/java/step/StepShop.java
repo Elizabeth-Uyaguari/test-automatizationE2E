8c10451cd0aa3d59f097c885f91d278f2a9d02f4
Elizabeth Uyaguari <ely.uyaguari@gmail.com>
2024-01-24 18:49:21 -0500

    Agregacion de metodo de espera

--- src/test/java/step/StepShop.java
+++ src/test/java/step/StepShop.java
@@ -27,41 +27,48 @@ public class StepShop {
 
     @Given("^(.*) go to main page")
     public void goMainPage(String actor) {
+        awaitLoadElements();
         Wait.until(() -> true).forNoMoreThan(5).seconds();
         OnStage.theActorCalled(actor).attemptsTo(Navigate.openPage());
     }
 
     @When("select product")
     public void selectProduct() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#content > div.row > div:nth-child(2) > div > div.image > a > img"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @Then("add product to cart")
     public void addProductToCart() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#button-cart"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @And("back to main page")
     public void backToMainPage() {
+        awaitLoadElements();
         OnStage.theActorInTheSpotlight().attemptsTo(Navigate.openPage());
     }
 
     @And("select second product")
     public void selectSecondProduct() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#content > div.row > div:nth-child(4) > div > div.image > a > img"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @And("select product color")
     public void selectProductColor() {
+        awaitLoadElements();
         Performable action = SelectFromOptions.byVisibleText("Red").from(By.cssSelector("#input-option226"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @Then("add product to cart again")
     public void addProductToCartAgain() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#button-cart"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
@@ -69,49 +76,53 @@ public class StepShop {
 
     @Then("go to shop cart")
     public void goToShopCart() {
+        awaitLoadElements();
         Performable action = Click.on(By.id("cart-total"));
         Performable action1 = Click.on(By.cssSelector("#cart > ul > li:nth-child(2) > div > p > a:nth-child(1) > strong"));
         OnStage.theActorInTheSpotlight().attemptsTo(action, action1);
     }
 
     @And("go to checkout")
-    public void goToCheckout() throws InterruptedException {
+    public void goToCheckout() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#content > div.buttons.clearfix > div.pull-right > a"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
-        Thread.sleep(2000);
     }
 
     @And("select guest checkout")
     public void selectGuestCheckout() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#collapse-checkout-option > div > div > div:nth-child(1) > div:nth-child(4) > label > input[type=radio]"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @And("continue to billing details")
     public void continueToBillingDetails() {
+        awaitLoadElements();
         Performable action = Click.on(By.id("button-account"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     public Performable fillOutInformation(String valor, String id) {
+        awaitLoadElements();
         return Enter.theValue(valor).into(By.id(id));
     }
 
 
     @And("fill out personal information {string}, {string}, {string}, {string}")
-    public void fillOutPersonalInformation(String nombre, String apellido, String correo, String telefono) throws InterruptedException {
+    public void fillOutPersonalInformation(String nombre, String apellido, String correo, String telefono) {
+        awaitLoadElements();
         OnStage.theActorInTheSpotlight().attemptsTo(
                 fillOutInformation(nombre, "input-payment-firstname"),
                 fillOutInformation(apellido, "input-payment-lastname"),
                 fillOutInformation(correo, "input-payment-email"),
                 fillOutInformation(telefono, "input-payment-telephone")
         );
-        Thread.sleep(2000);
     }
 
     @And("fill out address information {string}, {string}, {string}")
-    public void fillOutAddressInformation(String direccion1, String ciudad, String codigoPostal) throws InterruptedException {
-
+    public void fillOutAddressInformation(String direccion1, String ciudad, String codigoPostal) {
+        awaitLoadElements();
         Performable action = SelectFromOptions.byVisibleText("Ecuador").from(By.id("input-payment-country"));
         Performable action2 = SelectFromOptions.byVisibleText("Azuay").from(By.id("input-payment-zone"));
         OnStage.theActorInTheSpotlight().attemptsTo(
@@ -121,39 +132,46 @@ public class StepShop {
                 action,
                 action2
         );
-        Thread.sleep(2000);
     }
 
     @And("continue to delivery method")
     public void continueToDeliveryMethod() {
+        awaitLoadElements();
         Performable action = Click.on(By.id("button-guest"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @And("continue to Payment Method")
     public void continueToPaymentMethod() {
+        awaitLoadElements();
         Performable action = Click.on(By.id("button-shipping-method"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
     }
 
     @And("select terms and conditions and continue to confirm order")
     public void selectTermsAndConditions() {
+        awaitLoadElements();
         Performable action = Click.on(By.cssSelector("#collapse-payment-method > div > div.buttons > div > input[type=checkbox]:nth-child(2)"));
         Performable action1 = Click.on(By.id("button-payment-method"));
         OnStage.theActorInTheSpotlight().attemptsTo(action, action1);
     }
 
     @And("confirm order")
-    public void confirmOrder() throws InterruptedException {
+    public void confirmOrder() {
+        awaitLoadElements();
         Performable action = Click.on(By.id("button-confirm"));
         OnStage.theActorInTheSpotlight().attemptsTo(action);
-        Thread.sleep(2000);
     }
 
     @And("verify order")
     public void verifyOrder() {
+        awaitLoadElements();
         Target target = Target.the("Your order has been placed!").locatedBy("#content > h1");
         OnStage.theActorInTheSpotlight().attemptsTo(Ensure.that(Text.of(target)).isEqualTo("Your order has been placed!"));
+    }
+
+    private void awaitLoadElements() {
+        Wait.until(() -> true).forNoMoreThan(5).seconds();
     }
 
 }
